a47a1a1caca232377ab006bf306bf26566f43ab3
TGFRT <68918738+TGFRT@users.noreply.github.com>
2024-10-02 16:43:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from google_auth_st import add_auth
+st.warning("Inicia sesion para que puedas ver")
 add_auth():
 st.success("HOlIIIIIIIIIII")
 st.write(st.session_state.email)
